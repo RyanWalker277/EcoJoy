a833e83639e09a97723ad23a390ecf835cb6b279
RyanWalker277 <andianurag277@gmail.com>
2022-07-16 12:26:03 +0530

    Created Apps

--- Rakathon/settings.py
+++ Rakathon/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'contact_queries',
+    'news',
+    'plants',
+    'portfolio',
+    'testimonials',
 ]
 
 MIDDLEWARE = [
