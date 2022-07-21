cf7cc2e7dcc3c7947b6879179eb5a3de1639356a
RyanWalker277 <andianurag277@gmail.com>
2022-07-22 00:13:08 +0530

    added static root  in settings

--- Rakathon/settings.py
+++ Rakathon/settings.py
@@ -143,6 +143,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'media' , 'Static' ),
 ]
 
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
